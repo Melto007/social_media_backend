ec8097a3603f9e2b265a27807869ab636fc93cc2
Melto007 <meltosm8@gmail.com>
2024-03-17 12:24:35 +0530

    changes in following views

--- followers/views.py
+++ followers/views.py
@@ -1,7 +1,8 @@
 from rest_framework import (
     status,
     viewsets,
-    mixins
+    mixins,
+    exceptions
 )
 from rest_framework.response import Response
 from config import authentication
@@ -15,6 +16,7 @@ from core.models import (
 )
 from django.db.models import Q
 from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
 
 """ followers views class """
 class FollowersViewSet(
@@ -45,24 +47,69 @@ class FollowersViewSet(
 
 class FollowingViewSet(
     mixins.CreateModelMixin,
+    mixins.DestroyModelMixin,
+    mixins.ListModelMixin,
     viewsets.GenericViewSet
 ):
     serializer_class = FollowingSerializer
     authentication_classes = [authentication.JWTAuthentication]
     queryset = Follower.objects.all()
 
+    def list(self, request):
+        try:
+            follower = self.queryset.filter(user=request.user)
+            serializer = self.get_serializer(follower, many=True)
+
+            response = {
+                'data': serializer.data,
+                'status': status.HTTP_200_OK
+            }
+            return Response(response)
+        except Exception as e:
+            response = {
+                'message': e.args,
+                'status': status.HTTP_404_NOT_FOUND
+            }
+            return Response(response)
+
     def create(self, request):
         try:
             data = request.data
-            sender = data.get('sender', None)
-            profile = get_user_model().objects.get(name=sender)
+            follower = data.get('follower', None)
 
-            serializer = self.get_serializer(data=request.data)
+            profile = get_user_model().objects.get(name=follower)
+
+            serializer = self.get_serializer(data={'status': True})
             serializer.is_valid(raise_exception=True)
             serializer.save(user=request.user, following=profile)
 
+            follower = self.queryset.filter(user=request.user)
+            serializer = self.get_serializer(follower, many=True)
+
             response = {
-                'data': 'user followed successfully',
+                'message': 'followers created successfully',
+                'data': serializer.data,
+                'status': status.HTTP_200_OK
+            }
+            return Response(response)
+        except Exception as e:
+            response = {
+                'message': e.args,
+                'status': status.HTTP_404_NOT_FOUND
+            }
+            return Response(response)
+
+    def destroy(self, request, pk):
+        try:
+            follower = self.queryset.filter(user=request.user, following=pk).first()
+
+            if not follower:
+                raise exceptions.APIException('Followers not found')
+
+            follower.delete()
+
+            response = {
+                'data': 'follower deleted successfully',
                 'status': status.HTTP_200_OK
             }
             return Response(response)
